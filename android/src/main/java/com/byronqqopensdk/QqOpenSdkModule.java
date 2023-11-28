2afdcc1b8dcf16aa2b4e94e53e86986876f7cda2
ganghe <1250897253@qq.com>
2023-11-28 14:45:27 +0800

    升级targetSdkVersion到30版本，支持android QQ分享

--- android/src/main/java/com/byronqqopensdk/QqOpenSdkModule.java
+++ android/src/main/java/com/byronqqopensdk/QqOpenSdkModule.java
@@ -3,10 +3,22 @@
 package com.byronqqopensdk;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.ContentUris;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 
 import androidx.annotation.NonNull;
-
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.util.Log;
 import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
@@ -14,15 +26,18 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.UiThreadUtil;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.facebook.react.modules.core.RCTNativeAppEventEmitter;
+import com.tencent.connect.common.Constants;
+import com.tencent.connect.share.QQShare;
 import com.tencent.tauth.IUiListener;
 import com.tencent.tauth.Tencent;
 import com.tencent.tauth.UiError;
 
 import org.json.JSONObject;
-
-import com.tencent.connect.common.Constants;
 
 import java.util.Date;
 
@@ -31,6 +46,20 @@ public class QqOpenSdkModule extends ReactContextBaseJavaModule implements Activ
     private final ReactApplicationContext reactContext;
     public DeviceEventManagerModule.RCTDeviceEventEmitter eventEmitter;
     private Tencent mTencent;
+
+    private static final String NOT_INIT = "initApp required";
+    private static final String RCTQQShareTypeNews = "news";
+    private static final String RCTQQShareTypeImage = "image";
+    private static final String RCTQQShareTypeText = "text";
+    private static final String RCTQQShareTypeVideo = "video";
+    private static final String RCTQQShareTypeAudio = "audio";
+
+    private static final String RCTQQShareType = "type";
+    private static final String RCTQQShareText = "text";
+    private static final String RCTQQShareTitle = "title";
+    private static final String RCTQQShareDescription = "description";
+    private static final String RCTQQShareWebpageUrl = "webpageUrl";
+    private static final String RCTQQShareImageUrl = "imageUrl";
 
     public QqOpenSdkModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -98,6 +127,164 @@ public class QqOpenSdkModule extends ReactContextBaseJavaModule implements Activ
         mTencent.logout(reactContext);
         mTencent = null;
         eventEmitter = null;
+    }
+
+    @ReactMethod
+    public void shareToQQ(final ReadableMap data, final Promise promise) {
+        if (mTencent == null) {
+            promise.reject(NOT_INIT);
+            return;
+        }
+
+        UiThreadUtil.runOnUiThread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        _shareToQQ(data, 0);
+                        promise.resolve(null);
+                    }
+                }
+        );
+    }
+
+    private void _shareToQQ(ReadableMap data, int scene) {
+        String type = RCTQQShareTypeNews;
+        if (data.hasKey(RCTQQShareType)) {
+            type = data.getString(RCTQQShareType);
+        }
+
+        if (type.equals(RCTQQShareText)) {
+            try {
+                Intent shareIntent = new Intent();
+                ComponentName componentName = null;
+                if (scene == 0) {
+                    componentName =
+                            new ComponentName(
+                                    "com.tencent.mobileqq",
+                                    "com.tencent.mobileqq.activity.JumpActivity"
+                            );
+                } else if (scene == 1) {
+                    componentName =
+                            new ComponentName(
+                                    "com.qzone",
+                                    "com.qzonex.module.operation.ui.QZonePublishMoodActivity"
+                            );
+                } else {
+                    return;
+                }
+                shareIntent.setAction(Intent.ACTION_SEND);
+                shareIntent.putExtra(
+                        Intent.EXTRA_TEXT,
+                        data.getString(RCTQQShareTypeText)
+                );
+                shareIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                shareIntent.setType("text/plain");
+                shareIntent.setComponent(componentName);
+                getReactApplicationContext().startActivity(shareIntent);
+
+                WritableMap resultMap = Arguments.createMap();
+                resultMap.putString("type", "QQShareResponse");
+                resultMap.putInt("errCode", SHARE_RESULT_CODE_SUCCESSFUL);
+                resultMap.putString("message", "Share successfully.");
+                this.resolvePromise(resultMap);
+            } catch (Exception e) {
+                e.printStackTrace();
+
+                WritableMap resultMap = Arguments.createMap();
+                resultMap.putString("type", "QQShareResponse");
+                resultMap.putInt("errCode", SHARE_RESULT_CODE_FAILED);
+                resultMap.putString("message", "Share failed.");
+                this.resolvePromise(resultMap);
+            }
+            return;
+        }
+
+        Bundle bundle = new Bundle();
+        if (data.hasKey(RCTQQShareTitle)) {
+            bundle.putString(
+                    QQShare.SHARE_TO_QQ_TITLE,
+                    data.getString(RCTQQShareTitle)
+            );
+        }
+        if (data.hasKey(RCTQQShareDescription)) {
+            bundle.putString(
+                    QQShare.SHARE_TO_QQ_SUMMARY,
+                    data.getString(RCTQQShareDescription)
+            );
+        }
+        if (data.hasKey(RCTQQShareWebpageUrl)) {
+            bundle.putString(
+                    QQShare.SHARE_TO_QQ_TARGET_URL,
+                    data.getString(RCTQQShareWebpageUrl)
+            );
+        }
+        if (data.hasKey(RCTQQShareImageUrl)) {
+            bundle.putString(
+                    QQShare.SHARE_TO_QQ_IMAGE_URL,
+                    data.getString(RCTQQShareImageUrl)
+            );
+        }
+        if (data.hasKey("appName")) {
+            bundle.putString(QQShare.SHARE_TO_QQ_APP_NAME, data.getString("appName"));
+        }
+
+        if (type.equals(RCTQQShareTypeNews)) {
+            bundle.putInt(
+                    QQShare.SHARE_TO_QQ_KEY_TYPE,
+                    QQShare.SHARE_TO_QQ_TYPE_DEFAULT
+            );
+        } else if (type.equals(RCTQQShareTypeImage)) {
+            String image = data.getString(RCTQQShareImageUrl);
+            if (image.startsWith("content://") || image.startsWith("file://")) {
+                image = getImageAbsolutePath(getCurrentActivity(), Uri.parse(image));
+            }
+
+            bundle.putInt(
+                    QQShare.SHARE_TO_QQ_KEY_TYPE,
+                    QQShare.SHARE_TO_QQ_TYPE_IMAGE
+            );
+            bundle.putString(QQShare.SHARE_TO_QQ_IMAGE_LOCAL_URL, image);
+            bundle.putString(QQShare.SHARE_TO_QQ_IMAGE_URL, image);
+        } else if (type.equals(RCTQQShareTypeAudio)) {
+            bundle.putInt(
+                    QQShare.SHARE_TO_QQ_KEY_TYPE,
+                    QQShare.SHARE_TO_QQ_TYPE_AUDIO
+            );
+            if (data.hasKey("flashUrl")) {
+                bundle.putString(
+                        QQShare.SHARE_TO_QQ_AUDIO_URL,
+                        data.getString("flashUrl")
+                );
+            }
+        } else if (type.equals("app")) {
+            // TODO: 腾讯SDK 3.5.2.15 弃用了分享APP功能
+            //（模式4） 分享应用（已废弃）相关文档地址: https://wiki.connect.qq.com/%E5%88%86%E4%BA%AB%E6%B6%88%E6%81%AF%E5%88%B0qq%EF%BC%88%E6%97%A0%E9%9C%80qq%E7%99%BB%E5%BD%95%EF%BC%89
+            // bundle.putInt(QQShare.SHARE_TO_QQ_KEY_TYPE, QQShare.SHARE_TO_QQ_TYPE_DEFAULT);
+        }
+
+        Log.e("QQShare", bundle.toString());
+
+        if (scene == 0) {
+            // Share to QQ.
+            bundle.putInt(
+                    QQShare.SHARE_TO_QQ_EXT_INT,
+                    QQShare.SHARE_TO_QQ_FLAG_QZONE_ITEM_HIDE
+            );
+            mTencent.shareToQQ(getCurrentActivity(), bundle, this);
+        } else if (scene == 1) {
+            // Share to Qzone.
+            bundle.putInt(
+                    QQShare.SHARE_TO_QQ_EXT_INT,
+                    QQShare.SHARE_TO_QQ_FLAG_QZONE_AUTO_OPEN
+            );
+            mTencent.shareToQQ(getCurrentActivity(), bundle, this);
+        }
+    }
+
+    private void resolvePromise(ReadableMap resultMap) {
+        getReactApplicationContext()
+                .getJSModule(RCTNativeAppEventEmitter.class)
+                .emit("QQ_Resp", resultMap);
     }
 
     @Override
